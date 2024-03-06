ced2e44fa08da7346d131623c24204de1f9e6501
Neivanny1 <nevileneivanny@gmail.com>
2024-03-06 14:16:35 +0000

    added the main file of the program

---
+++ 0-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+TASK
+Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
+
+Returns an empty list if n <= 0
+You can assume n will be always an integer
+
+EXPECTED OUTPUT
+guillaume@ubuntu:~/0x00$ 
+guillaume@ubuntu:~/0x00$ ./0-main.py
+[1]
+[1,1]
+[1,2,1]
+[1,3,3,1]
+[1,4,6,4,1]
+guillaume@ubuntu:~/0x00$ 
+'''
+
+
+"""
+0-main file that runs code
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
