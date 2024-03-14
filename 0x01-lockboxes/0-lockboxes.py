bdee7f69f7f00d077c63da3299c8250ca877b1cc
Neivanny1 <nevileneivanny@gmail.com>
2024-03-14 05:45:58 +0000

    task 2 alx interview task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -16,3 +16,17 @@ Return True if all boxes can be opened, else return False
 
 '''
 
+def canUnlockAll(boxes):
+    if (type(boxes) is not list):
+        return False
+    if (len(boxes) == 0):
+        return False
+    keys = [0]
+    for i in keys:
+        for j in boxes[i]:
+            if j not in keys and j != i and j < len(boxes) and j != 0:
+                keys.append(j)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
