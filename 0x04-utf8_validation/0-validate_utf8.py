b34da83feb9959d44c863e749a826256470f823a
Neivanny1 <nevileneivanny@gmail.com>
2024-04-04 14:41:43 +0000

    added module documentation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,23 +13,30 @@ to handle the 8 least significant bits of each integer
 
 
 def validUTF8(data):
-    bytes_to_follow = 0
+    next_byte = 0
     for byte in data:
-        if bytes_to_follow > 0:
+        if next_byte > 0:
             if byte >> 6 != 0b10:
                 return False
-            bytes_to_follow -= 1
+            next_byte -= 1
         else:
+            '''
+            checking for:
+                1. single-byte character
+                2. two-byte character
+                3. three-byte character
+                4. four-byte character
+            '''
             if byte >> 7 == 0:
                 continue
             elif byte >> 5 == 0b110:
-                bytes_to_follow = 1
+                next_byte = 1
             elif byte >> 4 == 0b1110:
-                bytes_to_follow = 2
+                next_byte = 2
             elif byte >> 3 == 0b11110:
-                bytes_to_follow = 3
+                next_byte = 3
             else:
                 return False
-        if bytes_to_follow < 0:
+        if next_byte < 0:
             return False
-    return bytes_to_follow == 0
+    return next_byte == 0
