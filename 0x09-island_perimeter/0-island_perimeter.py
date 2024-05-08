692d7e793b10b733feeb2b22afceaa23e8ed764a
Neivanny1 <nevileneivanny@gmail.com>
2024-05-08 14:45:26 +0000

    removed parms

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,39 +1,15 @@
-#!/usr/bin/python3
-"""
-Task:
-    Create a function def island_perimeter(grid):
-    that returns the perimeter of the island described in grid:
-    grid is a list of list of integers:
-    0 represents water
-    1 represents land
-    Each cell is square, with a side length of 1
-    Cells are connected horizontally/vertically (not diagonally).
-    grid is rectangular, with its width and height not exceeding 100
-    The grid is completely surrounded by water
-    There is only one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t
-    connected to the water surrounding the island).
-"""
-
-
-def isWinner(x, nums):
-    """Check the winner of a prime game session with `x` rounds.
-    """
-    if x < 1 or not nums:
-        return None
-    marias_wins, bens_wins = 0, 0
-    n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if i == 1 or not is_prime:
-            continue
-        for j in range(i + i, n + 1, i):
-            primes[j - 1] = False
-    for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-    if marias_wins == bens_wins:
-        return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
+def island_perimeter(grid):
+    perimeter = 0
+    grid_length = len(grid)
+    for row in range(grid_length):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row - 1 < 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                if column - 1 < 0 or grid[row][column - 1] == 0:
+                    perimeter += 1
+                if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
+                    perimeter += 1
+                if row + 1 >= grid_length or grid[row + 1][column] == 0:
+                    perimeter += 1
+    return perimeter
