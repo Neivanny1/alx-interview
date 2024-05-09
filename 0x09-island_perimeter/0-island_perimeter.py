f786f38b2e6a532bbd1369f196c8f7a12bb003d2
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-05-09 14:47:47 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,15 +5,26 @@
 
 
 def island_perimeter(grid):
-    ret = 0
-    nrows = len(grid)
-    ncols = len(grid[0])
-    for i in range(nrows):
-        ret += sum(grid[i])*4
-        for j in range(ncols):
-            if grid[i][j] == 1:
-                if j + 1 < ncols and grid[i][j+1] == 1:
-                    ret -= 2
-                if i + 1 < nrows and grid[i+1][j] == 1:
-                    ret -= 2
-    return ret
+    total_perimeter = 0
+    for i, row in enumerate(grid):
+        for j, element in enumerate(row):
+            if (element == 0):
+                continue
+            if (j != 0 and row[j - 1] == 0):
+                total_perimeter += 1
+            if (j == 0):
+                total_perimeter += 1
+            if (j != len(row) - 1 and row[j + 1] == 0):
+                total_perimeter += 1
+            if (j == len(row) - 1):
+                total_perimeter += 1
+            if (i != 0 and grid[i - 1][j] == 0):
+                total_perimeter += 1
+            if (i == 0):
+                # top edge case
+                total_perimeter += 1
+            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
+                total_perimeter += 1
+            if (i == len(grid) - 1):
+                total_perimeter += 1
+    return total_perimeter
